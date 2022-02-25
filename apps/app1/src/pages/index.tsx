8087534015cf4ed21bfd3cc985f97f8dd8f25754
apple-yagi <applesryu@gmail.com>
2022-02-26 02:03:11 +0900

    format space

--- apps/app1/src/pages/index.tsx
+++ apps/app1/src/pages/index.tsx
@@ -2,14 +2,14 @@ import ReactDOM from 'react-dom';
 import { css } from '@emotion/react';
 
 const App = () => (
-    <h1
-      css={css`
-        text-align: center;
-        color: red;
-      `}
-    >
-      Hello World
-    </h1>
-  );
+  <h1
+    css={css`
+      text-align: center;
+      color: red;
+    `}
+  >
+    Hello World
+  </h1>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));

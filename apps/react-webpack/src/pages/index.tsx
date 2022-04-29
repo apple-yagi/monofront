3d84689b26dc3fb08939d43742abcea8bb60f27e
apple-yagi <applesryu@gmail.com>
2022-04-29 16:36:36 +0900

    Add recoil

--- apps/react-webpack/src/pages/index.tsx
+++ apps/react-webpack/src/pages/index.tsx
@@ -1,10 +1,20 @@
+import { todoState } from '@/states/todo';
 import { Link } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
 
-const IndexPage = () => (
-  <div>
-    <h1>IndexPage!!!</h1>
-    <Link to='/about.html'>About</Link>
-  </div>
-);
+const IndexPage = () => {
+  const [todo, setTodo] = useRecoilState(todoState);
+
+  return (
+    <div>
+      <h1>IndexPage!</h1>
+      <Link to='/about.html'>About</Link>
+      <div style={{ marginTop: '50px' }}>
+        <h3>Todo: {todo}</h3>
+        <input type='text' onChange={(e) => setTodo(e.target.value)} />
+      </div>
+    </div>
+  );
+};
 
 export default IndexPage;

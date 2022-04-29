961806cb2d24e256bc534634c0fc33fc391ce122
apple-yagi <applesryu@gmail.com>
2022-04-29 15:16:52 +0900

    Add  webpack analyzer

--- apps/react-webpack/webpack.prod.js
+++ apps/react-webpack/webpack.prod.js
@@ -1,5 +1,7 @@
 const { merge } = require('webpack-merge');
 const Dotenv = require('dotenv-webpack');
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const common = require('./webpack.common');
 const path = require('path');
 const nodeEnv = process.env.NODE_ENV || 'development';
@@ -27,6 +29,10 @@ module.exports = merge(common, {
   plugins: [
     new Dotenv({
       path: path.resolve(__dirname, `./config/.env.${nodeEnv}`),
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: false,
+      generateStatsFile: true,
     }),
   ].filter(Boolean),
 });

15da2b2f6167d30461cce8b79c7dde80d1ff9a4f
apple-yagi <applesryu@gmail.com>
2022-06-30 19:00:27 +0900

    workspace🚀

--- apps/react-esbuild/src/pages/index.tsx
+++ apps/react-esbuild/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
+import { Button } from '@ui/Button';
 
 const IndexPage = () => (
   <div>
     <h1>IndexPage</h1>
     <Link to='/about.html'>About</Link>
+    <Button></Button>
   </div>
 );
 
--- apps/react-webpack/src/pages/about.tsx
+++ apps/react-webpack/src/pages/about.tsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
+import { Button } from '@ui/Button';
 
 const AboutPage = () => (
   <div>
     <h1>About</h1>
     <Link to='/index.html'>Index</Link>
+    <Button></Button>
   </div>
 );
 
